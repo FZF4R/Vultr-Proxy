0cb78dc7b89f8dd63ef380d3c59738a9f5f2c8e8
Hung Vu <43626840+FZF4R@users.noreply.github.com>
2023-03-28 17:00:39 +0700

    .

--- SetupVPS.sh
+++ SetupVPS.sh
@@ -94,8 +94,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=10000
-COUNT = 300
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+COUNT=300
+LAST_PORT=$(($FIRST_PORT+$COUNT))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
